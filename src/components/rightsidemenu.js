938adf2f1b54e7fd927bcbd46a0f6ab863dfe657
vereslabs <robinson5000@freenet.de>
2017-05-01 11:53:40 +0200

    Updates

---
+++ src/components/rightsidemenu.js
@@ -0,0 +1,15 @@
+import React, { Component} from "react";
+
+export default class RightSideMenu extends Component {
+    
+    render(
+     
+        return ( 
+            <div className="rightSidemenu">
+                <h3>Right Side menu</h3>
+                <p><Link value="Hey"></Link> </p>
+            </div>
+        );
+    )
+    
+}
