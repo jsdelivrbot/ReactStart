d6fd6d917e89c9d3bdbf06343a5ef0260a1e037a
vereslabs <robinson5000@freenet.de>
2017-05-01 12:41:26 +0200

    x

--- src/components/rightsidemenu.js
+++ src/components/rightsidemenu.js
@@ -2,16 +2,13 @@ import React, { Component} from "react";
 
 export default class RightSideMenu extends Component {
     
-    render(
+    render() {
      
         return ( 
             <div className="rightSidemenu">
                 <h3>Right Side menu</h3>
-                <p>
-                    sss
-                </p>
             </div>
         );
-    )
+    }
     
 }
